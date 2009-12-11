d37f0171d5724849251984d32fc8cb0cf778ed25
Pascal Rettig <pascal@cykod.com>
2009-12-11 15:46:48 -0500

    Added support for write overlay messages from other modules

--- app/controllers/message/manage_controller.rb
+++ app/controllers/message/manage_controller.rb
@@ -66,6 +66,58 @@ class Message::ManageController < ModuleController
     render :partial => 'user'
   end
 
+
+  def write_overlay
+    @message = MessageMessage.new()
+
+    if request.post? && params[:message] 
+      @message.attributes = params[:message]
+      @message.from_user = myself
+      
+      valid_users = []
+      
+      if !params[:partial]
+        @message.valid?
+        @message.recipient_users.each do |usr|
+          valid_users << usr
+        end
+        if valid_users.length == 0
+          @message.errors.add(:recipient_ids,' must be selected')
+        end
+        
+        if @message.errors.length == 0
+          @message.save
+          @message.send_message(valid_users,:single => true)
+          flash.now[:message_sent] = true
+        end
+      end
+    end
+    
+    if params[:recipient_id]
+      @message.recipient_ids = params[:recipient_id].to_s.split(",").join("###")
+    end
+
+    categories = Message::AdminController.module_options.admin_message_category_list
+    if categories.length > 0
+      @message_templates = MessageTemplate.select_options(:conditions => ['category IN (?)',categories])
+    end
+
+    render :partial => 'write_overlay'
+  end
+
+  def autocomplete
+    mod_opts = Message::AdminController.module_options
+    if mod_opts.use_friends 
+      @suser = SocialUser.user(myself) 
+      @users = SocialUnit.find(:all,:conditions => ['social_unit_members.end_user_id=?', myself.id],:group => 'social_units.id', :joins => 'LEFT JOIN social_unit_members ON (social_unit_members.social_unit_id = social_units.id)' )
+      @users += SocialFriend.find(:all,:conditions => ["social_friends.end_user_id=?",myself.id],:group => 'end_users.id',:joins => [ :friend_user ]).collect { |usr| usr.friend_user }
+    else
+      @users = EndUser.find(:all, :order => 'last_name, first_name')
+    end
+    values = @users.map {  |elm| {  :caption => elm.name, :value => "end_user_#{elm.id}"} }
+    render :text => values.to_json()
+  end
+
   def load_template
     
     @message_template = MessageTemplate.find_by_id(params[:load_template_id]) 
